be4b8efd1efcce9230950f36c23a179175dfc541
LKIX <115419842+LKIX@users.noreply.github.com>
2024-07-07 20:31:09 -0400

    cambio de icono del juego

--- src/main/java/com/example/proyectouno_entrega_2/UnoApplication.java
+++ src/main/java/com/example/proyectouno_entrega_2/UnoApplication.java
@@ -3,20 +3,29 @@ package com.example.proyectouno_entrega_2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 public class UnoApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(UnoApplication.class.getResource("Juego.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1300, 700);
+        String filePath = "src/main/resources/images/icono.png";
+        Image icon = new Image(new File(filePath).toURI().toString());
+        stage.getIcons().add(icon);
+
+
         stage.setTitle("Uno!");
         stage.setScene(scene);
         stage.show();
     }
     public static void main(String[] args) {
+
         launch();
     }
 }
