db2208c4d34e644c71c19cc61b6039597de81b56
FernandL9 <faliendo.23@est.ucab.edu.ve>
2024-07-08 21:51:42 -0400

    Agregando funcionalidades al menú y botones

--- src/main/java/com/example/proyectouno_entrega_2/MenuController.java
+++ src/main/java/com/example/proyectouno_entrega_2/MenuController.java
@@ -5,9 +5,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import org.controlsfx.control.action.Action;
 import java.io.IOException;
+import javafx.scene.control.ButtonType;
 
 public class MenuController {
     @FXML
@@ -17,5 +21,18 @@ public class MenuController {
         Scene scene = new Scene(fxmlLoader.load(), 1300, 700);
         stage.setScene(scene);
         stage.show();
+        JuegoController.crear();
+    }
+    @FXML
+    public void Salir(ActionEvent event){
+        ButtonType buttonType=new ButtonType("a");
+        Alert alert=new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Salir");
+        alert.setHeaderText("¿Seguro que quieres cerrar del juego?");
+        alert.setContentText("Estas a punto de salir de la aplicación");
+        if(alert.showAndWait().get()== ButtonType.OK){
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.close();
+        }
     }
 }
